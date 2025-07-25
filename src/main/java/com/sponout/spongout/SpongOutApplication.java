efe315f8021672ca134dc44a5629f09dfbf10b27
Pawel-Mackiewicz <pawel@mackiewicz.info>
2025-07-25 18:34:45 +0200

    Initial commit

---
+++ src/main/java/com/sponout/spongout/SpongOutApplication.java
@@ -0,0 +1,13 @@
+package com.sponout.spongout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpongOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sponout/spongout/SpongOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sponout.spongout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
